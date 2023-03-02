43e28485cf436c0f1a4a0feda4b4d5f1b2b90d3a
gavin3567 <gavinostheimer00@gmail.com>
2023-03-01 20:24:52 -0700

    Add project files.

---
+++ Gavin_Ostheimer_Book_App/Models/IBookstoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gavin_Ostheimer_Book_App.Models
+{
+    public interface IBookstoreRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
---
+++ Gavin_Ostheimer_Book_App/Models/ViewModels/BooksViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gavin_Ostheimer_Book_App.Models.ViewModels
+{
+    public class BooksViewModel
+    {
+        public IQueryable<Book> Books { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
